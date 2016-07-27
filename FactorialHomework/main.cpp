7345c2084709a6c041d80c832db46f7ca744406e
maxineludington <maxine.ludington@gmail.com>
2016-07-27 09:04:46 -0500

    llll

--- FactorialHomework/main.cpp
+++ FactorialHomework/main.cpp
@@ -3,13 +3,32 @@
 //  FactorialHomework
 //
 //  Created by Maxine Ludington on 7/26/16.
-//  Copyright © 2016 Maxine Ludington. All rights reserved.
+//  Copyright © 2016 Maxine Ludington. All rights reserved.FUCK ME
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int factorial(int number);
+int number;
+
+int main()
+{
+//int number = 0; // intitialized
+cout << "Choose a number.";
+cin>> number;
+cout << factorial(number);
+}
+
+int factorial(int number)
+{
+    if (number<=1)
+    {
+        return 1;
+        
+    }
+else
+    {
+        return (factorial(number - 1) * number);
+    }
 }
